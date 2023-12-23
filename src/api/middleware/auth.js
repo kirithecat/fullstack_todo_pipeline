c437b0daf0bf7907f3486335489109b910821488
Ivan Karaman <4237268+ivanAndCode@users.noreply.github.com>
2023-12-24 10:40:33 +1300

    Fix export

--- src/api/middleware/auth.js
+++ src/api/middleware/auth.js
@@ -1,4 +1,8 @@
 
-export function isAuthorised() {
+function isAuthorised() {
   console.log('I am a middleware checking if user is authorised to talk to this API')
+}
+
+module.exports = {
+  isAuthorised
 }
