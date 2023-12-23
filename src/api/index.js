e24777abe5103cc5381f6bedf6505568c535d0dc
Ivan Karaman <4237268+ivanAndCode@users.noreply.github.com>
2023-12-24 10:36:21 +1300

    Add the future middleware placeholder

--- src/api/index.js
+++ src/api/index.js
@@ -2,52 +2,68 @@ const express = require('express')
 const https = require('https')
 const {readFileSync} = require("node:fs");
 const {getItems} = require("./db");
+const {isAuthorised} = require("./middleware/auth");
 //TODO: explore swagger generation & jsdoc annotations (when doing contracts)
 const app = express()
 const httpsPort = 443
 
-//using mkcert to generate key/certificate for localhost
+//using mkcert to generate key/certificate for localhost HTTPS
 //- https://github.com/FiloSottile/mkcert
 //- https://stackoverflow.com/a/54083405
 //------------------------------------------------
 //---IMPORTANT: mkcert installs local root CA!!!--
 //------------------------------------------------
-  const httpsOptions = {
-    key: readFileSync('./localhost/localhost+2-key.pem'),
-    cert: readFileSync('./localhost/localhost+2.pem'),
-    requestCert: false,
-    rejectUnauthorized: false
-  };
+const httpsOptions = {
+  key: readFileSync('./localhost/localhost+2-key.pem'),
+  cert: readFileSync('./localhost/localhost+2.pem'),
+  requestCert: false,
+  rejectUnauthorized: false
+};
 
-  app.get('/items/', (req, res) => {
-    const items = getItems()
-    res.send(items)
-  })
+//--------------------------------
+//---------middleware-------------
+//--------------------------------
+app.route('/*').all((req, res, next) => {
+  isAuthorised()
+  next()
+})
 
-  app.post('/items/', (req, res) => {
-    res.send('added an item!')
-  })
+//--------------------------------
+//----------routes----------------
+//--------------------------------
+app.get('/items/', (req, res) => {
+  const items = getItems()
+  res.send(items)
+})
 
-  app.get('/items/:index', (req, res) => {
-    res.send(`here is your item with ${req.params.index} index`)
-  })
+app.post('/items/', (req, res) => {
+  res.send('added an item!')
+})
 
-  app.delete('/items/:index', (req, res) => {
-    res.send(`deleted an item with an ${req.params.index} index`)
-  })
+app.get('/items/:index', (req, res) => {
+  res.send(`here is your item with ${req.params.index} index`)
+})
 
-  app.patch('/items/:index', (req, res) => {
-    res.send(`updated an item with an ${req.params.index} index`)
-  })
+app.delete('/items/:index', (req, res) => {
+  res.send(`deleted an item with an ${req.params.index} index`)
+})
 
-  app.post('/items/reset/default', (req, res) => {
-    res.send('reseted items to default list!')
-  })
+app.patch('/items/:index', (req, res) => {
+  res.send(`updated an item with an ${req.params.index} index`)
+})
 
-  app.post('/items/reset', (req, res) => {
-    res.send('reseted items!')
-  })
+app.post('/items/reset/default', (req, res) => {
+  res.send('reseted items to default list!')
+})
 
-  https.createServer(httpsOptions, app).listen(httpsPort, () => {
-    console.log(`Example app listening on port ${httpsPort}`)
-  })
+app.post('/items/reset', (req, res) => {
+  res.send('reseted items!')
+})
+
+
+//--------------------------------
+//------server start--------------
+//--------------------------------
+https.createServer(httpsOptions, app).listen(httpsPort, () => {
+  console.log(`Example app listening on port ${httpsPort}`)
+})
---
+++ src/api/middleware/auth.js
@@ -0,0 +1,4 @@
+
+export function isAuthorised() {
+  console.log('I am a middleware checking if user is authorised to talk to this API')
+}
