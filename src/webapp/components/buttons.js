6a3b0d89e3921e40faf5c83212a614261e8dcc79
Ivan Karaman <4237268+ivanAndCode@users.noreply.github.com>
2024-01-14 15:43:06 +1300

    Finished refactoring a second version of the app that uses components

---
+++ src/webapp/components/buttons.js
@@ -0,0 +1,11 @@
+class Buttons extends HTMLElement {
+  constructor() {
+    super();
+    this.innerHTML = `
+        <button type="reset" id="remove-all">Remove all tasks</button>
+        <button type="button" id="reset-default">Reset all tasks (default)</button>
+    `;
+  }
+}
+
+customElements.define('todo-buttons', Buttons);
