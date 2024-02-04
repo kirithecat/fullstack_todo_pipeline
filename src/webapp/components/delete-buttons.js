ed04fecb09e8db7813750c45d45a481e57e57c40
Ivan Karaman <4237268+ivanAndCode@users.noreply.github.com>
2024-02-04 13:32:49 +1300

    Fix a bug with deleting items in a component version

--- src/webapp/components/delete-buttons.js
+++ src/webapp/components/delete-buttons.js
@@ -10,8 +10,12 @@ export class DeleteButtons extends HTMLElement {
     const deleteButtonsArray = [].slice.call(deleteButtons);
     for (const deleteButton of deleteButtonsArray) {
       deleteButton.addEventListener('click', async (event) => {
+        console.log("adding an event listerner for a X button")
         const itemIndex = event.target.name
         await deleteItem(itemIndex)
+
+        const elementToDelete = document.getElementById(`delete-button-${itemIndex}`)
+        elementToDelete.parentElement.remove()
       });
     }
   }
