7fdfe72d1c9d3a0f54ca870dabac9a95d44caeff
Ivan Karaman <4237268+ivanAndCode@users.noreply.github.com>
2024-05-16 12:21:18 +1200

    Convert the rest of items controllers

--- src/api/routes/items.js
+++ src/api/routes/items.js
@@ -8,20 +8,6 @@ import {
 } from "../controllers/items.js";
 
 export const items = express.Router();
-import {getAllController} from "../controllers/get-all.js";
-import {weatherifyItem} from "../helpers/weather.js";
-import {addItem, deleteItem, getItem, updateItem} from "../db.js";
-
-items.get('/items/', getAllController.get);
-
-items.post('/items/', async (req, res) => {
-  const weatheredItem = await weatherifyItem(req.body.item)
-  res.send(addItem(weatheredItem))
-})
-
-items.get('/items/:index', (req, res) => {
-  res.send(getItem(req.params.index))
-})
 
 items.get('/items/', getItemsController.get);
 items.post('/items/', createItemController.post)
